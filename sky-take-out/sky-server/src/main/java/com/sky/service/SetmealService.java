01be898cf258b9d33ce344a792d1edd50852a99a
ocean-dragon <13553678527@163.com>
2026-02-28 17:56:24 +0800

    客户小程序端查看商品业务逻辑

--- sky-take-out/sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -4,6 +4,7 @@ import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.result.PageResult;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import org.springframework.stereotype.Service;
 
@@ -56,4 +57,20 @@ public interface SetmealService {
      * @param id
      */
     void enable(Integer status, Long id);
+
+    /**
+     * 条件查询
+     *
+     * @param setmeal
+     * @return
+     */
+    List<Setmeal> list(Setmeal setmeal);
+
+    /**
+     * 根据id查询菜品选项
+     *
+     * @param id
+     * @return
+     */
+    List<DishItemVO> getDishItemById(Long id);
 }
